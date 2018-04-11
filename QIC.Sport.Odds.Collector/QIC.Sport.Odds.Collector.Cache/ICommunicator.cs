548ec4e9c69d3043e35cd2b8b8de2756e6a197e1
Yeasaan <yeasaan8421@gmail.com>
2018-04-11 18:30:52 +0800

    add

---
+++ QIC.Sport.Odds.Collector/QIC.Sport.Odds.Collector.Cache/ICommunicator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ML.EGP.Sport.CommandProtocol.Command;
+
+namespace QIC.Sport.Odds.Collector.Cache
+{
+    public interface ICommunicator
+    {
+        void Send<T>(TakeServerCommand tcmd, T data) where T : class ,new();
+    }
+}
